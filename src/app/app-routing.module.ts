d62302528749abb750bbc2e3ce1cc3db200a8c15
Juan <juan.alesandri.acc@gmail.com>
2022-09-29 18:04:15 -0300

    Creacion de paginas para cada uno de los modulos e inicio de ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '', } //<---localhost:4200/
+  { path: 'auth', loadChildren: () => import(`./modules/auth/auth.module`).then(m => m.AuthModule) }, //<---localhost:4200/
+  {
+    path: '',
+    component: HomePageComponent,
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  }
+
 ];
 
 @NgModule({
