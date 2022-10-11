87a9a13b105acc121f8fba627da66164a3413b53
Juan <juan.alesandri.acc@gmail.com>
2022-10-11 11:56:59 -0300

    Se guarda token de sesion en cookie

--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '@modules/auth/services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-auth-page',
@@ -9,21 +10,27 @@ import { AuthService } from '@modules/auth/services/auth.service';
 })
 export class AuthPageComponent implements OnInit {
   formLogin: FormGroup = new FormGroup({});
+  sessionError: boolean = false;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private cookie: CookieService) { }
 
   ngOnInit(): void {
     this.formLogin = new FormGroup(
       {
-        email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [Validators.required, Validators.minLength(6)])
+        email: new FormControl('test@test.com', [Validators.required, Validators.email]),
+        password: new FormControl('12345678', [Validators.required, Validators.minLength(6)])
       }
     )
   }
 
   sendLogin(){
     const {email, password} = this.formLogin.value;
-    this.authService.sendCredentials(email, password);
+    this.authService.sendCredentials(email, password).subscribe(resp => {
+      console.log('Ok login');
+    }, err => {
+      this.sessionError = true;
+      console.log('Error login');
+    })
   }
 
 }
--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,13 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly urlBase = environment.api;
+  constructor(private httpClient: HttpClient, private cookie:CookieService) { }
 
-  constructor() { }
-
-  sendCredentials(email: string, password: string): void{
-    console.log(email, password);
+  sendCredentials(email: string, password: string): Observable<any> {
+    const credentials = {
+      email,
+      password
+    }
+    return this.httpClient.post(`${this.urlBase}/auth/login`, credentials)
+    .pipe(
+      tap((respOk: any) => {
+        const {tokenSession, data} = respOk;
+        this.cookie.set('token', tokenSession, 4, '/');
+      })
+    )
   }
 }
